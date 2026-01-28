85721e5d3e48e6a850a1986e5dbe30d6b7e8882e
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2026-01-28 17:12:37 -0600

    Created Admin SuperUser, and registered models

--- journals/admin.py
+++ journals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import projectEntry, journalEntry
+
 # Register your models here.
+admin.site.register(projectEntry)
+admin.site.register(journalEntry)
--- journals/models.py
+++ journals/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from django.db import models
 
 # Create your models here.
@@ -6,9 +7,15 @@ class projectEntry(models.Model):
     description = models.TextField()
     date_started = models.DateTimeField("date published")
     date_updated = models.DateTimeField("date updated")
+    def __str__(self):
+        return self.project_name
 
 class journalEntry(models.Model):
     header = models.CharField(max_length=255)
     content = models.CharField(max_length=255)
     date_pub = models.DateTimeField("date published")
     project_entry = models.ForeignKey(projectEntry, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.header
+    def was_published_recently(self):
+        return self.date_pub <= timezone.now() - datetime.timedelta(days=1)
