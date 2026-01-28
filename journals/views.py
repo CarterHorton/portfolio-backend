5f7da9bdfd8315e5dac8f56f7b3150de7e10621e
CarterHorton <137738454+CarterHorton@users.noreply.github.com>
2026-01-28 17:18:32 -0600

    created views

--- journals/views.py
+++ journals/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the index page for journals")
+    return HttpResponse("This is the index page for projects")
+
+def projects_journals(request, projectEntry_id):
+    return HttpResponse("This is the project list page for certian journals for: %s" % projectEntry_id)
+
+# This page won't actually exist btw its just for testing purposes
+def journal(request, journalEntry_id):
+    return HttpResponse("This is the journal page for entry: %s" % journalEntry_id)
